c291218512ee622e133d6be5fe11375f84c11b2c
michael <desarrollombg@hotmail.com>
2022-01-30 18:54:25 -0500

    config port heroku

--- index.js
+++ index.js
@@ -95,8 +95,9 @@ app.use((request,response)=>{
     })
 })
 
-// const PORT = process.env.PORT || 3001
-const PORT = 3001
+const PORT = process.env.PORT || 3001
+
+//const PORT = 3001
 //app.listen(PORT)
 
 app.listen(PORT, () => {
